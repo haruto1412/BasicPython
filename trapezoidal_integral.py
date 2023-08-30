af55c454ff59b1e60bfc47cd0fdce2e74e56c742
h.suzuki.23.6.1034@4nonome.com <h.suzuki.23.6.1034@4nonome.com>
2023-08-31 03:45:07 +0900

    制御構文の解答

--- trapezoidal_integral.py
+++ trapezoidal_integral.py
@@ -1,5 +1,22 @@
-from math import sin
+from math import sin,pi
 # --example--
 # print(sin(0))
 # >>> 0
 # -----------
+N=100
+
+a = 0
+b = 1 / (2 * pi)
+delta_x = (b - a) / N
+
+# 積分値の初期化
+integral = 0.0
+
+# 台形積分を計算
+for i in range(N):
+    x0 = a + i * delta_x
+    x1 = a + (i + 1) * delta_x
+    integral += (sin(x0) + sin(x1)) * delta_x / 2
+
+# 結果の出力
+print("sin(x)の[0, 1/2π]における積分値（台形積分）:", integral)
