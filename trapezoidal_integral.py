dcc4a8f3e433773d83ff8457d4605d19f08b85f6
h.suzuki.23.6.1034@4nonome.com <h.suzuki.23.6.1034@4nonome.com>
2023-10-29 22:10:54 +0900

    関数の解答

--- trapezoidal_integral.py
+++ trapezoidal_integral.py
@@ -7,80 +7,56 @@ from math import sin,pi,sqrt,exp
 
 
 # 台形積分を計算する関数
-def trapezoidal_integral_sin(a, b, n):
-    # 区間の幅
+import math
+
+def trapezoidal_integration(f, a, b, n):
     delta_x = (b - a) / n
-    # 積分値の初期化
-    integral = 0.0
-    # 台形積分を計算
+    approx_integral = 0
     for i in range(n):
-        x0 = a + i * delta_x
-        x1 = a + (i + 1) * delta_x
-        integral += (sin(x0) + sin(x1)) * delta_x / 2
+        x1 = a + i * delta_x
+        x2 = a + (i + 1) * delta_x
+        area = (delta_x / 2) * (f(x1) + f(x2))
+        approx_integral += area
+    return approx_integral
 
-    return integral
-# 区間 [0, π/2] の分割数
-n = 50
-# 区間 [0, π/2] における台形積分を計算
-result = trapezoidal_integral_sin(0, pi/2, n)
-# 結果の出力
-print("sin(x)の[0, π/2] における積分値（台形積分）:", result)
+# 課題1
+def task1():
+    def sin_function(x):
+        return math.sin(x)
+    
+    a = 0
+    b = math.pi / 2
+    n = 50
+    result = trapezoidal_integration(sin_function, a, b, n)
+    return result
 
+# 課題2
+def task2():
+    def func2(x):
+        return 4 / (1 + x**2)
+    
+    a = 0
+    b = 1
+    n = 100
+    result = trapezoidal_integration(func2, a, b, n)
+    return result
 
+# 課題3
+def task3():
+    def func3(x):
+        return math.sqrt(math.pi) * math.exp(-x**2)
+    
+    a = -100
+    b = 100
+    n = 1000
+    result = trapezoidal_integration(func3, a, b, n)
+    return result
 
-def trapezoidal_integral(f, a, b, n):
-    # 区間の幅
-    delta_x = (b - a) / n
+# 3つの課題を実行
+result1 = task1()
+result2 = task2()
+result3 = task3()
 
-    # 積分値の初期化
-    integral = 0.0
-
-    # 台形積分を計算
-    for i in range(n):
-        x0 = a + i * delta_x
-        x1 = a + (i + 1) * delta_x
-        integral += (f(x0) + f(x1)) * delta_x / 2
-
-    return integral
-
-# 対象の関数
-def target_function(x):
-    return 4 / (1 + x**2)
-
-# 区間 [0, 1] の分割数
-n = 100
-
-# 区間 [0, 1] における台形積分を計算
-result = trapezoidal_integral(target_function, 0, 1, n)
-
-# 結果の出力
-print("関数 4 / (1 + x^2) の [0, 1] における積分値（台形積分）:", result)
-
-
-def trapezoidal_integral(f, a, b, n):
-    # 区間の幅
-    delta_x = (b - a) / n
-
-    # 積分値の初期化
-    integral = 0.0
-
-    # 台形積分を計算
-    for i in range(n):
-        x0 = a + i * delta_x
-        x1 = a + (i + 1) * delta_x
-        integral += (f(x0) + f(x1)) * delta_x / 2
-
-    return integral
-
-# 対象の関数
-def target_function(x):
-    return sqrt(pi) * exp(-x**2)
-
-# 区間 [-100, 100] の分割数
-n = 1000
-
-# 区間 [-100, 100] における台形積分を計算
-result = trapezoidal_integral(target_function, -100, 100, n)
-
-# 結果の出力
-print("関数 √π * exp(-x^2) の [-100, 100] における積分値（台形積分）:", result)
+print("課題1: sin(x)関数の[0, π/2]における積分値の近似:", result1)
+print("課題2: 関数 4 / (1 + x^2) の[0, 1]における積分値の近似:", result2)
+print("課題3: 関数 √π * exp(-x^2) の[-100, 100]における積分値の近似:", result3)
